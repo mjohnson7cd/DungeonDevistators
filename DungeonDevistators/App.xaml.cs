00a01dd13590c8782fb910d018778fb34bea938b
mjohnson7cd <mjohnson7cd@gmail.com>
2023-08-14 12:19:58 -0500

    Added solution

---
+++ DungeonDevistators/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DungeonDevistators;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ DungeonDevistators/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonDevistators;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
